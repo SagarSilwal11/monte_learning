970f3ed58998ec9489081043bd67a50155f6aa7f
SagarSilwal11 <sagar.silwal002@gmail.com>
2025-04-15 16:47:47 +0545

    databaseadded

--- montelearning/settings.py
+++ montelearning/settings.py
@@ -79,16 +79,28 @@ WSGI_APPLICATION = "montelearning.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'pratice', 
+#         'USER': 'test', 
+#         'PASSWORD': 'Sagar@1234',
+#         'HOST': 'localhost',   
+#         'PORT': '3306',  
+#     }
+# }
+
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pratice', 
-        'USER': 'test', 
-        'PASSWORD': 'Sagar@1234',
-        'HOST': 'localhost',   
-        'PORT': '3306',  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 
 # Password validation
