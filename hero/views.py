f7efd1d10babe5a4f41b952f57c0f05daadcc86b
SagarSilwal11 <sagar.silwal002@gmail.com>
2025-04-17 17:03:30 +0545

    view added

--- hero/views.py
+++ hero/views.py
@@ -12,33 +12,47 @@ from common.pagination import CustomPageNumberPagination
 class HeroApi(APIView):
     serializer_class = HeroSerializers
 
+    
     def get(self, request, pk=None):
         if pk is None:
+            ALLOWED_PARAMS = {"status", "is_featured", "created_at", "search","page_size","page"}
+            unexpected = set(request.query_params.keys()) - ALLOWED_PARAMS
+            if unexpected:
+                return Response(
+                    {"detail": f"Unexpected query parameter(s): {', '.join(unexpected)}"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
             try:
                 heroes = Hero.objects.all().order_by("-id")
                 # Only sort by 'created_at' if passed in query params
-                created_at=request.query_params.get("created_at",None)
-                if created_at:
-                    if created_at=="latest":
-                        heroes=heroes.order_by("-created_at")
-                    elif created_at=="oldest":
-                        heroes=heroes.order_by("created_at")
-                    elif created_at=="ascending":
-                        heroes=heroes.order_by("heading")
-                    elif created_at=="descending":
-                        heroes=heroes.order_by("-heading")
-                
-                status_filter=request.query_params.get("status",None)
-                if status_filter is not None:
-                    try:
-                        if status_filter.lower()=='true':
-                            heroes=heroes.filter(status=True)
-                        elif status_filter.lower()=='false':
-                            heroes=heroes.filter(status=False)
+                try:
+                    created_at=request.query_params.get("created_at",None)
+                    if created_at:
+                        if created_at=="latest":
+                            heroes=heroes.order_by("-created_at")
+                        elif created_at=="oldest":
+                            heroes=heroes.order_by("created_at")
+                        # elif created_at=="ascending":
+                        #     heroes=heroes.order_by("heading")
+                        # elif created_at=="descending":
+                        #     heroes=heroes.order_by("-heading")
                         else:
-                            return Response ({"detail":'Invalid value for status'},status=status.HTTP_400_BAD_REQUEST)
-                    except:
-                        return Response({'detail':"Invalid value"},status=status.HTTP_400_BAD_REQUEST)
+                            heroes=heroes.order_by("-id")
+                except Exception as e:
+                    return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+                try:
+                    status_filter=request.query_params.get("status",None)
+                    if status_filter is not None:
+                            if status_filter.lower()=='true':
+                                heroes=heroes.filter(status=True)
+                            elif status_filter.lower()=='false':
+                                heroes=heroes.filter(status=False)
+                            else:
+                                return Response ({"detail":'Invalid value for status'},status=status.HTTP_400_BAD_REQUEST)
+                except Exception as e:
+                        return Response({'detail':"str(e)"},status=status.HTTP_400_BAD_REQUEST)
                 
                 featured_filter=request.query_params.get("is_featured",None)
                 if featured_filter is not None:
@@ -53,10 +67,16 @@ class HeroApi(APIView):
                             },status=status.HTTP_400_BAD_REQUEST)
                     except:
                         return Response({'detail':"Invalid value"},status=status.HTTP_400_BAD_REQUEST)
+            
+                try:
+                    search = request.query_params.get("search", None)
+                    if search:
+                            heroes = heroes.filter(heading__istartswith=search)
+                except Exception as e:
+                    return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-                
-                if not heroes:
-                    return Response({'detail': 'No heroes found'}, status=status.HTTP_404_NOT_FOUND)
+                # if not heroes:
+                #     return Response({'details': 'No heroes found'}, status=status.HTTP_404_NOT_FOUND)
                 #applying the paginator
                 paginator=CustomPageNumberPagination()
                 paginated_queryset=paginator.paginate_queryset(heroes,request)
@@ -73,6 +93,7 @@ class HeroApi(APIView):
                 return Response({'detail': 'Hero not found'}, status=status.HTTP_404_NOT_FOUND)
             except Exception as e:
                 return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 
     def post(self, request):
         try:
