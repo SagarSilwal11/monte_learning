abb316fd06f1902aac6dbbb25d83d4c9dfdbfc76
SagarSilwal11 <sagar.silwal002@gmail.com>
2025-04-21 12:20:48 +0545

    image validation

--- common/serializers.py
+++ common/serializers.py
@@ -1,7 +1,21 @@
-from common.models import Image
+from common.models import Image,ImageContent
 from rest_framework import serializers
 
 class ImageSerailizer(serializers.ModelSerializer):
     class Meta:
         model=Image
         fields="__all__"
+
+class ImageContentSerializer(serializers.ModelSerializer):
+    image=ImageSerailizer()
+    class Meta:
+        model=ImageContent
+        fields=[
+            "id",'relation_type',
+            'image'
+        ]
+
+    def create(self,validated_data):
+        image_data=validated_data.pop('image')
+        image=Image.objects.create(**image_data)
+        return ImageContent.objects.create(image=image,**validated_data)
