98bbca09bd5425fecd9c400b94fdaefdd1fd4e8f
SagarSilwal11 <sagar.silwal002@gmail.com>
2025-04-27 18:42:15 +0545

    facilityserialzier update

--- facilities/serializers.py
+++ facilities/serializers.py
@@ -1,14 +1,29 @@
-from facilities.models import Facilities
 from rest_framework import serializers
-
-class FacilitySerializer(serializers.ModelSerializer):
-    # icon=serializers.SerializerMethodField()#convert path to full url automaticaally
-    # def get_icon(self,obj):
-    #     request=self.context.get("request")#ensure the request exists
-    #     if obj.icon and request:
-    #         return request.build_absolute_uri(obj.icon.url)
-    #     return None  # return None if no
-    icon=serializers.ImageField(use_url=True)
+from activities.models import ActivitiesModel
+from django.core.exceptions import ValidationError
+from django.conf import settings
+class ActivitiesModelSerializers(serializers.ModelSerializer):
+    # image=serializers.SerializerMethodField()# custom method for full url
+    # def get_image(self,obj):
+    #     request=self.context.get("request")# get request context
+    #     if obj.image:
+    #         return request.build_absolute_uri(obj.image.url)
+    #     else:
+    #         return None
+    image=serializers.ImageField(use_url=True)
+    short_content=serializers.SerializerMethodField()
     class Meta:
-        model=Facilities
-        fields="__all__"
+        model=ActivitiesModel
+        fields=['id',
+            'image', 'heading', 'content', 'created_at', 'updated_at', 'is_featured', 'status', 'keywords', 'description', 'slug','short_content']
+
+    def get_short_content(self,obj):
+        return obj.content[:100] + '...' if len(obj.content)> 100 else obj.content
+
+
+    def validate_content(self,value):
+        if len(value)<=10:
+          raise ValidationError("Please provide the content longer the 10 characters")
+        if len(value) > 200:
+            raise ValidationError("Please provide a content shorter than 200 characters.")
+        return value
